1f4247303030cc4c11561f85311d052fa1f2a210
Jeff Henrikson <jeff@groovescale.com>
2020-10-01 19:11:46 -0700

    refactor: extract framework.scala

--- src/main/scala/recschemeq/lunch.scala
+++ src/main/scala/recschemeq/lunch.scala
@@ -1,23 +1,5 @@
 package recschemeq
 
-import scala.reflect.ClassTag
-
-trait ImplFor[U,V] {
-  //def apply(u:U) : V
-  def build(u:U) : V
-}
-
-trait Magic {
-  /**
-    * Search for an appropriate implementation of configuration a with type t.
-    * May throw exception when a is an invalid configuration.
-    * @param a a configuration
-    * @param t a desired class for the return type
-    * @return a runtime instantiation of the configuration with the desired class
-    */
-  def lookup[A,T](a:A)(implicit t:ClassTag[T]) : T
-}
-
 trait Food {}
 
 case class Orange(name:String) extends Food
